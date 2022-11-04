c08e08e92cc6cf9a6a2b14b480f0128da752e07e
Jaime Trevino <trevinojaime42@gmail.com>
2022-11-03 21:24:24 -0500

    navigation bar

---
+++ navigation/screens/MainContainer.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { View, Text} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+//Screens
+import HomeScreen from './HomeScreen'
+import SettingsScreen from './SettingsScreen'
+
+//Screen names
+const homeName = 'Home';
+const settingsName = 'Settings'
+
+const Tab = createBottomTabNavigator();
+
+export default function MainContainer() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+            initialRouteName={homeName}
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                let iconName;
+                let rn = route.name;
+
+                if (rn === homeName) {
+                    iconName = focused ? 'home' : 'home-outline'
+                } else if(rn === settingsName) {
+                    iconName = focused ? 'settings' : 'settings-outline'
+                }
+                return <Ionicons name={iconName} size={size} color = {color}/>
+                },
+            })}>
+                <Tab.Screen name={homeName} component = {HomeScreen}/>
+                <Tab.Screen name={settingsName} component = {SettingsScreen}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
